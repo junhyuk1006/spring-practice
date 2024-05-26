5348fed086a7bd9177c3b30a60376b44498a40ed
jun <whdid3766@gmail.com>
2024-05-26 13:12:56 +0900

    05.26

--- practice1/src/main/java/com/example/practice1/web/validation/ValidationItemController.java
+++ practice1/src/main/java/com/example/practice1/web/validation/ValidationItemController.java
@@ -1,2 +1,37 @@
-package com.example.practice1.web.validation;public class ValidationItemController {
+package com.example.practice1.web.validation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/validation/items")
+public class ValidationItemController {
+
+    @GetMapping
+    public String itemList(){
+        return "validation/items";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "validation/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(){
+        return "validation/item";
+    }
+
+    @GetMapping("/edit")
+    public String editForm(){
+        return "validation/editForm";
+    }
+
+    @PostMapping("/edit")
+    public String edit(){
+        return "validation/items";
+    }
+
 }
